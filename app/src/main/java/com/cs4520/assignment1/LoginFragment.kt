1f3d32c740de528b21b7d8a7219beb7519c44b7c
aulin1 <audrey.youran.lin@gmail.com>
2024-02-09 11:20:37 -0500

    Added navgraph to main activity

--- app/src/main/java/com/cs4520/assignment1/LoginFragment.kt
+++ app/src/main/java/com/cs4520/assignment1/LoginFragment.kt
@@ -1,7 +1,13 @@
 package com.cs4520.assignment1
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
 class LoginFragment : Fragment(R.layout.login_fragment){
-
+        //TODO: put something here
+        //TODO: check out viewbinding
+        //TODO: figure out navgraph
 }
